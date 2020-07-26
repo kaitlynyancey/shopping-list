f100324ef4ba7ccb896b6da4e01d04509b4b9eb4
Kaitlyn Yancey <kyancey423@gmail.com>
2020-07-26 15:07:17 -0400

    updated with javascript code

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 $(function(){
-    $('.shopping-item-toggle').on('click', function(){
-        $('span').toggleClass('.shopping-item_checked');
-
-
+    $('#js-shopping-list-form').submit(function(event){
+        event.preventDefault();
+        const addItem = $('input').val();
+        const listUpdate = $(`<li><span class="shopping-item">${addItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div></li>`)
+        $('.shopping-list').append(listUpdate);
+        $('input').val("");
+    });
+    
+    $('.shopping-list').on('click','.shopping-item-toggle', function(event){
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
     });
 
+    $('.shopping-list').on('click','.shopping-item-delete', function(event){
+        $(this).closest('li').remove();
+    });
 
-
+    
 
 });
